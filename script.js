289ad854f87cb746876c2df30bcc358c962885e8
LataBansavade <139533645+LataBansavade@users.noreply.github.com>
2023-11-26 13:01:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // Array of API URLs to fetch data from
+
+
+// Array of API URLs to fetch data from
 const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/1",
   "https://jsonplaceholder.typicode.com/todos/2",
@@ -12,4 +15,34 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
 
-// You can write your code here
+// Function to fetch data from an API URL and measure time taken
+async function fetchData(url) {
+  const startTime = Date.now();
+  const response = await fetch(url);
+  const endTime = Date.now();
+  const timeTaken = endTime - startTime;
+  return timeTaken;
+}
+
+// Function to display time taken for Promise.all() and Promise.any()
+async function displayTimeTaken() {
+  const promiseAllStartTime = Date.now();
+  const allPromises = apiUrls.map(fetchData);
+  const allResults = await Promise.all(allPromises);
+  const promiseAllEndTime = Date.now();
+  const promiseAllTimeTaken = promiseAllEndTime - promiseAllStartTime;
+
+  const promiseAnyStartTime = Date.now();
+  const anyPromises = apiUrls.map(fetchData);
+  await Promise.any(anyPromises).catch(() => {}); // Promise.any may reject if all promises reject
+  const promiseAnyEndTime = Date.now();
+  const promiseAnyTimeTaken = promiseAnyEndTime - promiseAnyStartTime;
+
+  // Display time taken in the respective HTML elements
+  document.getElementById('output-all').innerText = `${promiseAllTimeTaken} ms`;
+  document.getElementById('output-any').innerText = `${promiseAnyTimeTaken} ms`;
+}
+
+// Call the function to display time taken
+displayTimeTaken();
+
